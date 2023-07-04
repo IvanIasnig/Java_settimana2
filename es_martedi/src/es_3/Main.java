42c06f19f91f1114ccd51df759d50d52480c0247
ivan1998iasnig@gmail.com <ivan1998iasnig@gmail.com>
2023-07-04 16:56:42 +0200

    ok

--- es_martedi/src/es_3/Main.java
+++ es_martedi/src/es_3/Main.java
@@ -1,9 +1,18 @@
 package es_3;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Map<Integer, Contatti> contacts = new HashMap<>();
+		
+		Contatti cont1 = new Contatti ("Mario",351744191, 1);
+		Contatti cont2 = new Contatti ("Luigi",351748191, 2);
+		Contatti cont3 = new Contatti ("Waluigi",351748911, 3);
+		Contatti cont4 = new Contatti ("Wario",351481911, 4);
 
 	}
 
